f66578db067e391bd23dc5c72f1a7b224590639d
vedu2811 <vedant28j@gmail.com>
2025-12-16 14:49:36 +0530

    Max of three strings

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main{
     public static void main(String[] args) {
         Max m=new Max();
-        System.out.println(m.findMax(3.1f,4.2f,5.4f));
-        System.out.println(m.findMax(3f,5f,4f));
-        System.out.println(m.findMax(5.2f,4f,3f));
+        System.out.println(m.findMax("Apple", "Peach", "Banana"));
+        System.out.println(m.findMax("Apple", "Banana","Peach"));
+        System.out.println(m.findMax("Peach","Apple",  "Banana"));
     }
 }
--- src/Max.java
+++ src/Max.java
@@ -1,6 +1,6 @@
 public class Max {
-    public Float findMax(Float n1,Float n2,Float n3){
-        Float max=n1;
+    public String findMax(String n1,String n2,String n3){
+        String max=n1;
         if(n2.compareTo(n1)>0){
             max=n2;
         }
