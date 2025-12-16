ac86ed905fa499a434c2147942cef02886ae3002
vedu2811 <vedant28j@gmail.com>
2025-12-16 14:35:24 +0530

    Added : UC1 - Max of 3 integers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main{
     public static void main(String[] args) {
-
+        Max m=new Max();
+        System.out.println(m.findMax(3,4,5));
+        System.out.println(m.findMax(3,5,4));
+        System.out.println(m.findMax(5,4,3));
     }
 }
--- src/Max.java
+++ src/Max.java
@@ -1,2 +1,12 @@
 public class Max {
+    public Integer findMax(Integer n1,Integer n2,Integer n3){
+        Integer max=n1;
+        if(n2.compareTo(n1)>0){
+            max=n2;
+        }
+        if(n3.compareTo(max)>0){
+            max=n3;
+        }
+        return max;
+    }
 }
