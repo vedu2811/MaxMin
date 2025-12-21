6a5b9cc8782f84e3e9287f823e6c9d2c7ac6f6ea
vedu2811 <vedant28j@gmail.com>
2025-12-21 23:17:24 +0530

    Added : UC4 - Adding values array as parameter

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main{
     public static void main(String[] args) {
         Max m=new Max();
         System.out.println(m.findMax("Apple", "Peach", "Banana"));
-        System.out.println(m.findMax("Apple", "Banana","Peach"));
-        System.out.println(m.findMax(4,6,7));
-        System.out.println(m.findMax(2.33,6.1,0.4));
+        System.out.println(m.findMax("Apple", "Banana"));
+        System.out.println(m.findMax(4,6,3,4,5,7,9));
+        System.out.println(m.findMax(2.33,6.1,0.4,8.2,8.8));
     }
 }
--- src/Max.java
+++ src/Max.java
@@ -1,12 +1,20 @@
+import java.util.Arrays;
+
 public class Max  {
-    public <T extends Comparable<T>> T findMax(T n1,T n2,T n3){
-        T max=n1;
-        if(n2.compareTo(n1)>0){
-            max=n2;
+    public <T extends Comparable<T>> T findMax(T... values){
+        if(values.length==0){
+            return null;
         }
-        if(n3.compareTo(max)>0){
-            max=n3;
-        }
-        return max;
+        Arrays.sort(values);
+        return values[values.length-1];
+
+//        T max=n1;
+//        if(n2.compareTo(n1)>0){
+//            max=n2;
+//        }
+//        if(n3.compareTo(max)>0){
+//            max=n3;
+//        }
+//        return max;
     }
 }
