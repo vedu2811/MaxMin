0d880b18fd0c535cf193265d1635902875d56eea
vedu2811 <vedant28j@gmail.com>
2025-12-21 23:12:50 +0530

    Added : Refactor-2 (Was already present)

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main{
         System.out.println(m.findMax("Apple", "Peach", "Banana"));
         System.out.println(m.findMax("Apple", "Banana","Peach"));
         System.out.println(m.findMax(4,6,7));
+        System.out.println(m.findMax(2.33,6.1,0.4));
     }
 }
